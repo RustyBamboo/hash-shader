04fc6ed9dd99527be2b2108ee9f75a55ef5ceed8
RustyBamboo <d@volya.xyz>
2021-04-07 18:21:55 +0000

    Start a benchmark suite - Made a main cuda file which takes 1 argument - Python script to test variable number of hashing:

--- c/src/main.cu
+++ c/src/main.cu
@@ -3,20 +3,23 @@
 
 // linking cuda too hard
 #include "sha256.cu"
-
-static int test_idx = 0;
-void test_sha256(unsigned char *d, int d_len, char *answer, int N);
+void run_sha256(unsigned char *d, int d_len, int N);
 
 __global__ void kernel(unsigned char *d, int d_len, unsigned char *out) {
   SHA256(d, d_len, out);
 }
 
-int main() {
-  test(test_sha256, (SHA256_DIGEST_LENGTH * 2));
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+	printf("Must have at least one argument\n");
+	return -1;
+  }
+  // TODO: work on several at strings at once
+  run_sha256((unsigned char *)argv[1], strlen(argv[1]), (SHA256_DIGEST_LENGTH * 2));
   return 0;
 }
 
-void test_sha256(unsigned char *d, int d_len, char *answer, int N) {
+void run_sha256(unsigned char *d, int d_len, int N) {
   cudaDeviceSynchronize();
   unsigned char *d_c;
   cudaMallocManaged((void **)&d_c, d_len);
@@ -39,16 +42,8 @@ void test_sha256(unsigned char *d, int d_len, char *answer, int N) {
   for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
     sprintf(res + i * 2, "%02x", digest_c[i]);
   }
-  if (strcmp(res, answer) != 0) {
-    fprintf(stderr,
-            "cuda sha256 failed test %d\ngot:\n\t%s\n expected:\n\t%s\n\n",
-            test_idx, res, answer);
-  } else {
-    fprintf(stdout,
-            "cuda sha256 passed test %d\ngot:\n\t%s\n expected:\n\t%s\n\n",
-            test_idx, res, answer);
-  }
-  test_idx += 1;
+  fprintf(stdout, "%s\n", res);
+
   cudaFree(d_c);
   cudaFree(digest_c);
 }
---
+++ tests.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import subprocess
+import time
+
+tests = {
+    'abc': ['abc'],
+    'rust': ['rust is cool'],
+    'multiple_3': ['1', '2', '3'],
+    'multiple_50': [f"{i}XD" for i in range(50)],
+    'multiple_250': [f"{i}XD" for i in range(250)],
+    'multiple_1000': [f"{i}XD" for i in range(1000)],
+    'multiple_10000': [f"{i}XD" for i in range(10000)],
+}
+
+def run(func, *args, **kwargs):
+    start = time.time()
+    out = func(*args, **kwargs)
+    end = time.time()
+    return end - start, out
+
+if __name__ == '__main__':
+    vulkan_runner = './vulkan/target/release/vulkan'
+    cuda_runner = './c/cuda_test'
+
+    elapsed_times = []
+
+    for name, params in tests.items():
+        print(f"Running {name}")
+        elapsed_cuda, _ = run(subprocess.check_output, f"{cuda_runner} {' '.join(params)}", shell=True)
+        elapsed_vulkan, _ = run(subprocess.check_output, f"{vulkan_runner} {' '.join(params)}", shell=True)
+        elapsed_times.append([elapsed_cuda, elapsed_vulkan])
+
+    print("{:<30}{:<30}".format("CUDA", "VULKAN"))
+    for e in elapsed_times:
+        print("{:<30}{:<30}".format(*e))
+
